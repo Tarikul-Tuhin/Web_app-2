f0d7a7c7c3d5e02780890092c7acfc8db6725ca8
Tarikul-Tuhin <tituhinkh@gmail.com>
2021-12-09 16:26:21 +0600

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
-import 'package:flutter/material.dart';
 
-void main() {
+
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
+
+
+
+void main() async {
+  
+
   runApp(MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   @override
@@ -11,184 +22,276 @@ class MyApp extends StatelessWidget {
   }
 }
 
+
+
+
+final _firestore = FirebaseFirestore.instance;
+
 class HomePage extends StatefulWidget {
+
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+
+  String? name;
+  String? qty;
+  String? details;
+
+
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(),
         body: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                padding: EdgeInsets.all(50.0),
-                child: Row(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: EdgeInsets.all(50.0),
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        width: 120.0,
+                      ),
+                      Expanded(
+                        flex: -1,
+                        child: Column(
+                          children: [
+                             SizedBox(
+                              //height: 120.0,
+                            ),
+                            Container(
+                              width: 400.0,
+                              child: TextField(
+                                onChanged: (value) {
+                                  name = value;
+                                },
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  labelText: 'Name',
+                                  contentPadding: EdgeInsets.symmetric(vertical: 0.0, horizontal: 10.0),
+                                ),
+                              ),
+                            ),
+                             SizedBox(
+                              height: 10.0,
+                            ),
+                            Container(
+                              width: 400.0,
+                              child: TextField(
+                                onChanged: (value) {
+                                  qty = value;
+                                },
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  labelText: 'Qty',
+                                  contentPadding: EdgeInsets.symmetric(vertical: 0.0, horizontal: 10.0),
+                                ),
+                              ),
+                            ),
+                             SizedBox(
+                              height: 10.0,
+                            ),
+                            Container(
+                              width: 400.0,
+                              child: TextField(
+                                onChanged: (value) {
+                                  details = value;
+                                },
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  labelText: 'Details',
+                                  contentPadding: EdgeInsets.symmetric(vertical: 50, horizontal: 10),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                          flex: 1,
+                          child: Container()),
+                      Expanded(
+                        flex: 6,
+                        child: Image.network(
+                          'https://i.picsum.photos/id/48/200/200.jpg?hmac=3FKJwSlm1FM1GD916vZX2Z3HUjHsUXvQM3rYWYXsQvc',
+                          height: 232.5,
+                          width: 250,
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                      Expanded(
+                        flex: 8,
+                        child: Container(
+                        ),
+                      ),
+
+                    ],
+                  ),
+                ),
+                Row(
                   children: [
-                    SizedBox(
-                      width: 120.0,
+                     SizedBox(
+                      width: 168.0,
                     ),
-                    Column(
-                      children: [
-                        SizedBox(
-                            //height: 120.0,
-                            ),
-                        Container(
-                          width: 400.0,
-                          child: const TextField(
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: 'Name',
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 10.0,
-                        ),
-                        Container(
-                          width: 400.0,
-                          child: const TextField(
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: 'Qty',
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 10.0,
-                        ),
-                        Container(
-                          width: 400.0,
-                          child: const TextField(
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: 'Details',
-                              contentPadding: EdgeInsets.symmetric(
-                                  vertical: 50, horizontal: 10),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      width: 20.0,
-                    ),
-                    Image.network(
-                      'https://i.picsum.photos/id/48/200/200.jpg?hmac=3FKJwSlm1FM1GD916vZX2Z3HUjHsUXvQM3rYWYXsQvc',
-                      height: 232.5,
-                      width: 250,
-                      fit: BoxFit.fill,
+                    ElevatedButton.icon(
+                      onPressed: () {
+                        _firestore.collection('message').add({
+                          'name': name,
+                          'quantity': qty,
+                          'details': details,
+
+                        });
+                        //print(messageText);
+                      },
+                      icon: Icon(Icons.save),
+                      label: Text('Save'),
                     ),
                   ],
                 ),
-              ),
-              Row(
-                children: [
-                  SizedBox(
-                    width: 168.0,
-                  ),
-                  ElevatedButton.icon(
-                    onPressed: () {},
-                    icon: Icon(Icons.save),
-                    label: Text('Save'),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 50.0,
-              ),
-              Row(
-                children: [
-                  SizedBox(
-                    width: 167.0,
-                  ),
-                  Container(
-                    width: 400.0,
-                    child: const TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: '🔍 Search',
+                 SizedBox(
+                  height: 50.0,
+                ),
+                Row(
+                  children: [
+                     SizedBox(
+                      width: 167.0,
+                    ),
+                    Expanded(
+                      flex: 3,
+                      child: Container(
+                        width: 400.0,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: '🔍 Search',
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    width: 700.0,
-                  ),
-                  ElevatedButton.icon(
-                    onPressed: () {},
-                    icon: Icon(Icons.download),
-                    label: Text('Download'),
-                  ),
-                ],
-              ),
-              Container(
-                padding: EdgeInsets.only(left: 167.0),
-                child: Row(
-                  children: [
-                    DataTable(
-                      columns: const <DataColumn>[
-                        DataColumn(
-                          label: Text(
-                            'Name',
-                            style: TextStyle(fontStyle: FontStyle.italic),
-                          ),
-                        ),
-                        DataColumn(
-                          label: Text(
-                            'Age',
-                            style: TextStyle(fontStyle: FontStyle.italic),
-                          ),
-                        ),
-                        DataColumn(
-                          label: Text(
-                            'Role',
-                            style: TextStyle(fontStyle: FontStyle.italic),
-                          ),
-                        ),
-                      ],
-                      rows: const <DataRow>[
-                        DataRow(
-                          cells: <DataCell>[
-                            DataCell(Text('Sarah')),
-                            DataCell(Text('19')),
-                            DataCell(Text('Student')),
-                          ],
-                        ),
-                        DataRow(
-                          cells: <DataCell>[
-                            DataCell(Text('Janine')),
-                            DataCell(Text('43')),
-                            DataCell(Text('Professor')),
-                          ],
-                        ),
-                        DataRow(
-                          cells: <DataCell>[
-                            DataCell(Text('Janine')),
-                            DataCell(Text('43')),
-                            DataCell(Text('Professor')),
-                          ],
-                        ),
-                        DataRow(
-                          cells: <DataCell>[
-                            DataCell(Text('William')),
-                            DataCell(Text('27')),
-                            DataCell(Text('Associate Professor')),
-                          ],
-                        ),
-                      ],
+                    Expanded(
+                      flex: 8,
+                      child: Container(
+
+                      ),
                     ),
+                    Expanded(
+                      flex: 2,
+                      child: Container(
+                        height: 50.0,
+                        child: ElevatedButton.icon(
+                          onPressed: () {},
+                          icon: Icon(Icons.download),
+                          label: Text('Download'),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                        flex: 2,
+                        child: Container()),
                   ],
                 ),
-              ),
-            ],
+                Container(
+                  padding: EdgeInsets.only(left: 167.0),
+                  child: Row(
+                    children: [
+                      DataTable(
+                        columns:  <DataColumn>[
+                          DataColumn(
+                            label: Text(
+                              'SL',
+                              style: TextStyle(fontStyle: FontStyle.italic),
+
+                            ),
+                          ),
+                          DataColumn(
+                            label: Text(
+                              'Name',
+                              style: TextStyle(fontStyle: FontStyle.italic),
+                            ),
+                          ),
+                          DataColumn(
+                            label: Text(
+                              'Qty',
+                              style: TextStyle(fontStyle: FontStyle.italic),
+                            ),
+                          ),
+                          DataColumn(
+                            label: Text(
+                              'Details',
+                              style: TextStyle(fontStyle: FontStyle.italic),
+                            ),
+                          ),
+                          DataColumn(
+                            label: Text(
+                              'IMG',
+                              style: TextStyle(fontStyle: FontStyle.italic),
+                            ),
+                          ),
+                        ],
+                        rows:  <DataRow>[
+                          DataRow(
+                            cells: <DataCell>[
+                              DataCell(Text('1')),
+                              DataCell(Text('Burger')),
+                              DataCell(Text('2.5 KG')),
+                              DataCell(Text('Naga Burger with Chicken')),
+                              DataCell(Image.network('https://i.picsum.photos/id/981/200/200.jpg?hmac=LRIdIKhx3zHhDGgKVGHBdRjNRrgVbpWUw02O7Uuy2C4',
+                                height: 40.0, width: 40.0,)),
+
+                            ],
+                          ),
+
+                          DataRow(
+                            cells: <DataCell>[
+                              DataCell(Text('2')),
+                              DataCell(Text('Burger')),
+                              DataCell(Text('2.5 KG')),
+                              DataCell(Text('Naga Burger with Chicken')),
+                              DataCell(Image.network('https://i.picsum.photos/id/981/200/200.jpg?hmac=LRIdIKhx3zHhDGgKVGHBdRjNRrgVbpWUw02O7Uuy2C4',
+                                height: 40.0, width: 40.0,)),
+                            ],
+                          ),
+                          DataRow(
+                            cells: <DataCell>[
+                              DataCell(Text('3')),
+                              DataCell(Text('Burger')),
+                              DataCell(Text('2.5 KG')),
+                              DataCell(Text('Naga Burger with Chicken')),
+                              DataCell(Image.network('https://i.picsum.photos/id/981/200/200.jpg?hmac=LRIdIKhx3zHhDGgKVGHBdRjNRrgVbpWUw02O7Uuy2C4',
+                                height: 40.0, width: 40.0,)),
+                            ],
+                          ),
+                          DataRow(
+                            cells: <DataCell>[
+                              DataCell(Text('4')),
+                              DataCell(Text('Burger')),
+                              DataCell(Text('2.5 KG')),
+                              DataCell(Text('Naga Burger with Chicken')),
+                              DataCell(Image.network('https://i.picsum.photos/id/981/200/200.jpg?hmac=LRIdIKhx3zHhDGgKVGHBdRjNRrgVbpWUw02O7Uuy2C4',
+                                height: 40.0, width: 40.0,)),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
+
+
